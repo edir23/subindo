615525b1d3917a3329e9342c51c5d276d93266ed
Aluno <aluno@edu.senac.br>
2023-12-19 20:44:41 -0300

    MediaPonderada

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+double nota1, nota2, nota3;
+double media;
+
+Console.WriteLine("Digite a primeira nota:");
+nota1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite a segunda nota:");
+nota2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite a terceira nota:");
+nota3 = Convert.ToDouble(Console.ReadLine());
+
+
+media = (nota1 + nota2 + nota3) / 3;
+if (media >= 70)
+{
+    Console.WriteLine("Aprovado por média");
+}
+
+else
+{
+    Console.WriteLine("Reprovado");
+}
